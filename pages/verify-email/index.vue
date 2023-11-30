b8dee5b7893348724da8fcbc6e225bac7aa34d69
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-30 10:31:01 +0800

    Update index.vue

--- pages/verify-email/index.vue
+++ pages/verify-email/index.vue
@@ -5,6 +5,11 @@ definePageMeta({
 </script>
 
 <template>
+      <Head>
+    <Title>FAMSI Website | Email Verification</Title>
+    <Meta name="description" />
+  </Head>
+  
   <NuxtLoadingIndicator />
   <AuthVerifyEmailPage />
 </template>
