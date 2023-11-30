2cabb35ca315cd36ea7b02fdd1027d84bf0ee96c
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-30 09:33:05 +0800

    Create Link.vue

---
+++ components/base/Link.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  type: String,
+  disabled: Boolean,
+  href: String,
+});
+</script>
+
+<template>
+  <a
+    :href="props.href"
+    class="text-sm text-center rounded-full hover:shadow-md hover:shadow-[#0c66ee]/50 transition duration-300"
+  >
+    <slot></slot>
+</a>
+</template>
