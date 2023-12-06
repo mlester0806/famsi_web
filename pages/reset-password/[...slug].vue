8afc2353318421fcd5d342c42bedb5eb8df94b22
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-12-06 11:28:34 +0800

    Add files via upload

---
+++ pages/reset-password/[...slug].vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, reactive } from 'vue';
+import { useAuthStore } from '@/store/useAuthStore';
+
+definePageMeta({
+  layout: 'verify-pass',
+});
+
+const auth = useAuthStore();
+const {params, query} = useRoute();
+
+if(!params.slug[0] && !query?.email) {
+  navigateTo('/login');
+} 
+
+</script>
+
+<template>
+      <Head>
+    <Title>FAMSI Reset Password</Title>
+    <Meta name="description" />
+  </Head>
+    <NuxtLoadingIndicator />
+  <AuthResetPassPage />
+</template>
+
+
