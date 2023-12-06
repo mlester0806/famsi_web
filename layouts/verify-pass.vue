fbb6a66610906fcceab7d9e88317115c40bf4729
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-12-06 11:26:50 +0800

    Create verify-pass.vue

---
+++ layouts/verify-pass.vue
@@ -0,0 +1,11 @@
+<template>
+    <client-only>
+        <div class="min-h-screen font-sans antialiased relative bg-gray-100">
+      <div class="relative">
+        <main class="text-neutral-800">
+          <slot />
+        </main>
+      </div>
+    </div>
+    </client-only>
+</template>
