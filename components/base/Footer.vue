0c7aebeaaf4480f7c230351a923a87289f059007
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-12-06 09:29:14 +0800

    Update Footer.vue

--- components/base/Footer.vue
+++ components/base/Footer.vue
@@ -78,7 +78,7 @@
     <div
       class="py-8 sm:py-4 text-center text-sm text-[#666666] hover:text-gray-900"
     >
-      &copy; Copyright 2022 NEFA LLC. All rights reserved
+      &copy; Copyright 2023 FAMSI Inc. All rights reserved.
     </div>
   </footer>
 </template>
