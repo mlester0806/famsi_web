c4fae0292e24ac18ba735f5084d96a635f588880
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-12-06 11:28:17 +0800

    Add files via upload

---
+++ pages/forgot-password/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+definePageMeta({
+  layout: 'auth',
+});
+</script>
+
+<template>
+      <Head>
+    <Title>FAMSI Forgot Password</Title>
+    <Meta name="description" />
+  </Head>
+  <NuxtLoadingIndicator />
+  <AuthForgotPassPage />
+</template>
