4847e65aeda5bc06e5875a0bff4c9b4756efb8e2
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2024-01-10 05:51:39 +0800

    Update AuthenticatedNavbar.vue

--- components/base/AuthenticatedNavbar.vue
+++ components/base/AuthenticatedNavbar.vue
@@ -78,7 +78,7 @@ const handleSettingsModal = () => {
                         <a href="/portal/my-submitted-files">
               <button
                 class="block px-4 w-full py-3 text-sm text-gray-700 text-left transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-blue-600">
-                My Submitted Files
+                My Documents
               </button>
             </a>
 
