ce63dddbc19ea29376078ed221b3e5da2ba9fc31
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2024-01-11 23:23:54 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   alias: {
-    assets: '/<rootDir>/assets',
+    assets: "/<rootDir>/assets",
   },
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
-  css: ['~/assets/css/main.css'],
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-vue3-google-signin",
+  ],
+  googleSignIn: {
+    clientId:
+      "830590197481-nv6dphmkas77qob9mrop11dtoip5hfif.apps.googleusercontent.com",
+  },
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -16,8 +25,8 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap',
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap",
         },
       ],
     },
